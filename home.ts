9f2076fbd7976d70ffdc0704103d12c83647a6eb
Khoo Yit Keng <30973917+kyk85@users.noreply.github.com>
2018-02-24 11:31:09 +0800

    v0.2

---
+++ home.ts
@@ -0,0 +1,43 @@
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { LocationProvider } from '../../providers/location/location'
+
+declare var google;
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+ 
+  constructor(public navCtrl: NavController,
+    public locProv:LocationProvider) {
+
+  }
+
+  ionViewDidLoad(){
+    this.locProv.getUserLocation().then((resp)=>{
+      this.loadMap(resp.coords.latitude, resp.coords.longitude)
+    }).catch((error)=>{
+      console.log(error)
+    })
+    
+  }
+ 
+  loadMap(lat,long){
+ 
+    let latLng = new google.maps.LatLng(lat, long);
+ 
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+ 
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+ 
+  }
+}
